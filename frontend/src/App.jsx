250aebfe8b6c9169151bbdba010a27b4d3079570
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-28 21:03:38 +0530

    Create the components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import AnimatedBackground from "./components/AnimatedBackground";
-// import Dashboard from "./pages/Dashboard";
-
-export default function App() {
-  return (
-    <div className="min-h-screen text-white">
-      <AnimatedBackground />
-      {/* <Dashboard /> */}
+import Button from "./components/Button";
+function App() {
+  return <div>
+      <Button />
+      
     </div>
-  );
 }
+
+export default App;
---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,5 @@
+export default function Button(){
+    return <div className="bg-amber-500">
+        <button>Start Here</button>
+    </div>
+}
