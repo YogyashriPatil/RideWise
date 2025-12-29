c96d5b5ddaab74af28002d141a23132deceb7a9a
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-29 20:42:59 +0530

    Navbar created

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,5 @@
+export default function Navbar({text}){
+    return <div className="ridewise-font">
+        <button className="ridewise-line-btn">{text}</button>
+    </div>
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import NeonWaveBackground from "../background/NeonWaveBackground";
+import Navbar from "../components/Navbar";
+import Logo from "../logo/Logo";
 
 export default function Home(){
     return <div>
         <NeonWaveBackground />
+        <div style={{
+            position: "fixed",
+            zIndex: 9999,
+            color: "white"
+        }}>
+            {/* <Logo /> */}
+        </div>
+        <div className="flex gap-3">
+            <Navbar text="Day Prediction" />
+            {/* <Navbar text="Hour Prediction" /> */}
+
+        </div>
     </div>
 }
