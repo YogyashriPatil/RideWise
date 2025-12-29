faf8ccd6a19dbc5dd98b7606a1fdd11a6d0bbc46
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-29 20:30:27 +0530

    Make the home page skelton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 // import NeonWaveBackground from "./background/NeonWaveBackground";
 // import Button from "./components/Button";
 // import Logo from "./logo/Logo";
+import Home from "./pages/Home";
 import Landing from "./pages/Landing";
 function App() {
   return <div>
-      <Landing />    
+      {/* <Landing />     */}
+      <Home />
     </div>
 }
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+import NeonWaveBackground from "../background/NeonWaveBackground";
+
+export default function Home(){
+    return <div>
+        <NeonWaveBackground />
+    </div>
+}
