d85181fec6c09521f3c7bbd21967367fc1d02e10
YogyashriPatil <yogyashripatil20@gmail.com>
2025-12-30 17:52:36 +0530

    Explore the multiple background

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 // import NeonWaveBackground from "./background/NeonWaveBackground";
 // import Button from "./components/Button";
 // import Logo from "./logo/Logo";
+import RideWiseBackground from "./background/NewBackground";
+import RideWiseBackground from "./background/New1";
+
 import Home from "./pages/Home";
 import Landing from "./pages/Landing";
 function App() {
   return <div>
       {/* <Landing />     */}
-      <Home />
+      {/* <Home /> */}
+      <RideWiseBackground1 />
     </div>
 }
 
