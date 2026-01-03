e835cf5fd1f0983d552fe19fc16641c87e5b4269
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-03 20:06:33 +0530

    Make the button stylish

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,34 @@
-export default function Button({text,icon}){
-    return <div >
-        <button className="rounded-2xl bg-blue-800 h-10 w-40">{text}</button>
-    </div>
+export default function Button({ text }) {
+  return (
+    <button
+      className="
+        flex items-center justify-center
+        min-w-2 
+        text-[20px] md:text-[24px]
+        font-sans text-white
+        cursor-pointer select-none
+        rounded-lg
+        bg-[linear-gradient(144deg,#AF40FF,#5B42F3_50%,#00DDEB)]
+        shadow-[0_15px_30px_-5px_rgba(151,65,252,0.2)]
+        hover:outline-none active:outline-none
+      "
+    >
+      <span
+        className="
+          w-full h-full
+          px-6 py-4
+          rounded-md
+          bg-[rgb(5,6,45)]
+          transition-all duration-300
+          hover:bg-transparent
+        "
+      >
+        {text}
+      </span>
+    </button>
+  );
 }
+
 
 {/* <button className="group px-8 py-4 bg-white/10 backdrop-blur-sm text-white font-semibold rounded-full border-2 border-white/20 transition-all duration-300 hover:bg-white/20 hover:scale-105 hover:shadow-xl flex items-center gap-2">
             <MapPin className="w-5 h-5" />
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -18,3 +18,5 @@ export const LoginPage = () => {
         </RideWiseBackground>
     </div>
 }
+
+
