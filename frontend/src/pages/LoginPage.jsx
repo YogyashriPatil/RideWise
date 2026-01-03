de18ca8f8969c9281af0709bc0a310f9104c3ef4
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-03 19:55:57 +0530

    solving the some issue

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -6,6 +6,7 @@ export const LoginPage = () => {
     return <div className="relative min-h-screen w-full overflow-hidden">
         <RideWiseBackground>
             <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-8 text-center">
+                <div className="text-4xl pb-4">Login</div>
                 <div className="flex flex-col gap-6">
                     <InputBox text={"text"} placeholder="Enter Username" />
                     <InputBox text={"text"} placeholder="Enter Password" />
