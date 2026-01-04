56f4277cc1d7b1c07362936b4e73399f299333dc
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-04 20:40:16 +0530

    MAke the home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter , Route, Routes } from "react-router-dom";
-import { LoginPage } from "./pages/LoginPage";
-import { SignUp } from "./pages/SignUp";
 import Landing1 from "./pages/Landinge1";
 import { AuthPage } from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
@@ -8,8 +6,6 @@ function App() {
     return <BrowserRouter>
         <Routes>
             <Route path="/" element={<Landing1 />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUp />} />
             <Route path="/Auth" element={<AuthPage />} />
             <Route path="/home" element={<HomePage />} />
         </Routes>
