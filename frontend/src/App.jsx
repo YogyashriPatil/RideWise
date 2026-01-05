c2b51f9d059faeb5f35d0b448c6448a06276dd16
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-05 20:25:24 +0530

    Add the contact page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { BrowserRouter , Route, Routes } from "react-router-dom";
 import Landing1 from "./pages/Landinge1";
 import { AuthPage } from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
-import DashBoard from "./pages/DashBoard";
 import Predict from "./pages/Predict";
 import MapPage from "./pages/MapPage";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 function App() {
     return <BrowserRouter>
         <Routes>
@@ -15,6 +15,7 @@ function App() {
             <Route path="/predict" element={<Predict />} />
             <Route path="/map" element={<MapPage />} />
             <Route path="/aboutus" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
         
         </Routes>
     </BrowserRouter>
