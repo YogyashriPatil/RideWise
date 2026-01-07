f6919913676cf2e99b28499221d7f111537d4917
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-07 08:44:46 +0530

    Add the profile and update routes

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,7 +3,33 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 const router = express.Router();
+router.get("/me", async (req, res) => {
+  try {
+    const { userId } = req.query; // temporary (JWT later)
 
+    const user = await User.findById(userId).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch profile" });
+  }
+});
+router.put("/update", async (req, res) => {
+  try {
+    const { userId, name, email } = req.body;
+
+    const user = await User.findByIdAndUpdate(
+      userId,
+      { name, email },
+      { new: true }
+    ).select("-password");
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ message: "Profile update failed" });
+  }
+});
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
