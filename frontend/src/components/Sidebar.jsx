662a0e325ad7b7b0e51a2064ded88a5e7ef32a37
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-09 09:06:07 +0530

    CHange the sidebar layout and background

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,8 +3,17 @@ import { NavLink } from "react-router-dom";
 
 export default function SideNavbar() {
   return (
-    <aside className="w-64 h-screen pt-28 px-4 border-r border-white/10">
-      <h3 className="text-gray-500 text-xs mb-4">NAVIGATION</h3>
+    <aside className=" fixed top-0 left-0 h-screen w-[260px]
+    bg-gradient-to-b
+      from-[#6d3aa8]/35
+      via-[#3b155f]/30
+      to-[#12001f]/40
+    backdrop-blur-xl
+    border-r border-white/10
+    shadow-[inset_-1px_0_0_rgba(255,255,255,0.05)]
+    pt-28 px-4
+    z-40">
+      <p className="text-gray-500 text-xs mb-4 tracking-widest">NAVIGATION</p>
 
       <SidebarItem to="/home" icon={<LayoutDashboard />} text="Dashboard" />
       <SidebarItem to="/predict" icon={<Brain />} text="AI Insights" />
