2d6d44f5a2070bec4466278a5accf9e295a7f452
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-15 19:04:22 +0530

    Adding the contact us backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ import connectDB from "./config/db.js";
 import stationRoutes from "./routes/station.routes.js";
 import reservationRoutes from "./routes/reservation.routes.js";
 import chatRoutes from "./routes/chatRoutes.js";
-
+import contactRoutes from "./routes/contact.routes.js";
 import userRoutes from "./routes/auth.routes.js";
 import predictionRoutes from "./routes/prediction.routes.js";
 
@@ -27,6 +27,7 @@ app.use("/api/stations", stationRoutes);
 app.use("/api/reservations", reservationRoutes);
 app.use("/api/predict", predictionRoutes);
 app.use("/api/user", userRoutes)
+app.use("/api/contact", contactRoutes);
 app.use("/api", chatRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
