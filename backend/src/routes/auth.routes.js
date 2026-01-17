92adc14e11e2bc5d36b37934709103568f0d55d1
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-17 14:27:48 +0530

    Add the forgot password feature

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,6 +3,29 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
 const router = express.Router();
+router.post("/forgot-password", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields required" });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.json({ message: "Password updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 router.get("/me", async (req, res) => {
   try {
     const { userId } = req.query; // temporary (JWT later)
