b73f45c8e35d6a1c6290cbd3f8834114ac303201
YogyashriPatil <yogyashripatil20@gmail.com>
2026-01-18 20:41:53 +0530

    Make the reservation backend

--- backend/src/routes/reservation.routes.js
+++ backend/src/routes/reservation.routes.js
@@ -4,11 +4,22 @@ import {
   getMyReservations,
   cancelReservation,
 } from "../controllers/reservation.controller.js";
+import Reservation from "../models/Reservation.js";
 
 const router = express.Router();
 
-router.post("/", createReservation);
+router.post("/create", createReservation);
 router.get("/my", getMyReservations);
 router.delete("/:id", cancelReservation);
+router.get("/my/:userId",  async (req, res) => {
+  try {
+    const reservations = await Reservation.find({
+      userId: req.params.userId
+    }).sort({ createdAt: -1 });
 
+    res.json(reservations);
+  } catch (err) {
+    res.status(500).json({ message: "Failed to fetch reservations" });
+  }
+});
 export default router;
